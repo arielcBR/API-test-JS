dc126d73b2cdd4553d4e90c9023a16565d5cfe86
Ariel Campos <ariel.campos94@yahoo.com>
2023-02-13 09:22:43 -0300

    added new pets

--- test/pet.spec.js
+++ test/pet.spec.js
@@ -6,6 +6,8 @@ const petId = 7894;
 const req = supertest(urlBase);
 
 describe('PetStore Swagger - PET', () => {
+    const pets = require("../vendors/newPetN");
+
     it("POST pet", () => {
         const jsonFile = require('../vendors/pet.json');
         return req
@@ -17,9 +19,7 @@ describe('PetStore Swagger - PET', () => {
                 assert.equal(res.body.name, "Chulo");
             });
     });
-});
 
-describe('PetStore Swagger - PET', () => { 
     it("GET pet", () => {
         return req
             .get(`/pet/${petId}`)
@@ -28,24 +28,20 @@ describe('PetStore Swagger - PET', () => {
                 assert.equal(res.body.name, "Chulo");
             });
     });
-});
-
-describe("PetStore Swagger - PET", () => {
+    
     it("PUT pet", () => {
         const jsonFileUpdated = require("../vendors/petUpdate.json");
         return req
-            .put("/pet")
-            .send(jsonFileUpdated)
-            .then((res) => {
+        .put("/pet")
+        .send(jsonFileUpdated)
+        .then((res) => {
                 assert.equal(res.statusCode, 200);
                 assert.equal(res.body.status, "unavailable");
                 assert.equal(res.body.tags[0].name, "vaccinated");
                 assert.equal(res.body.tags[1].name, "castrated");
             });
-    });
-});
+        });
 
-describe("PetStore Swagger - PET", () => {
     it("DEL pet", () => {
         return req
             .del(`/pet/${petId}`)
@@ -53,6 +49,57 @@ describe("PetStore Swagger - PET", () => {
                 assert.equal(res.statusCode, 200);
             });
     });
+
+    
+    // Função de carga de animais - Setup
+    pets.array.forEach((pet => {
+        let { nomePet, idPet, nomeCategoria, idCategoria } = pet;
+        
+        it(`Setup Swagger - Add pet`, () => {
+            return req
+                .post("/pet")
+                .send({
+                    "id": idPet,
+                    "category": {
+                        "id": idCategoria,
+                        "name": nomeCategoria
+                    },
+                    "name": nomePet,
+                    "tags": [
+                        {
+                            "name": "unvaccinated"
+                        }
+                    ],
+                    "status": "available"
+                })
+                .then((res) => {
+                    assert.equal(res.statusCode, 200);
+                }); 
+        });
+    })); // Fim forEach
+
+    it("GET pet", () => {
+        return req
+            .get(`/pet/108436`)
+            .then((res) => {
+                assert.equal(res.statusCode, 200);
+                assert.equal(res.body.name, "Bola");
+            });
+    });
+    
+    // Função para deletar animais
+    pets.array.forEach((pet => {
+        let { idPet } = pet;
+
+        it(`Setup Swagger - Del pet`, () => {
+            return req
+                .del(`/pet/${idPet}`)
+                .then((res) => {
+                    assert.equal(res.statusCode, 200);
+                });
+        });
+    })); // Fim forEach
+
 });
-
-
+    
+    
---
+++ vendors/newPetN.js
@@ -0,0 +1,27 @@
+module.exports.array = [
+    {
+        "nomePet": "Bolt",
+        "idPet": "123456",
+        "nomeCategoria": "Dog",
+        "idCategoria": 1
+    },
+    {
+        "nomePet": "Pepsi",
+        "idPet": "98765",
+        "nomeCategoria": "Dog",
+        "idCategoria": 1
+    },
+    {
+        "nomePet": "Chulo",
+        "idPet": "56749",
+        "nomeCategoria": "Cat",
+        "idCategoria": 2
+    },
+    {
+        "nomePet": "Bola",
+        "idPet": "108436",
+        "nomeCategoria": "Dog",
+        "idCategoria": 1
+    }
+]
+
